11a04875b57fda5f941b8187b0c3179f646506ee
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-17 16:05:20 -0500

    agrega imagenes x DOM imagenes.js

---
+++ assets/js/imagenes.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function(){
+
+  var imagenes = ["panda adulto", "rostro del panda", "panda en arbol", "panda saludando"];
+
+  function addImage(array){
+    array.forEach(function(e){
+      var imagenesPanda = document.getElementById("imagenes-panda");
+
+      var image = document.createElement("img");
+      image.setAttribute("src","assets/img/"+ e + ".jpg");
+      image.setAttribute("title", e);
+      image.setAttribute("width","200");
+      imagenesPanda.appendChild(image);
+
+    })
+  };
+  addImage(imagenes);
+
+})
