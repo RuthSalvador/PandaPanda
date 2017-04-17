523b1e94bc85122c8f93571e156cfb5f97e616ef
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-17 18:01:26 -0500

    alineando div textos

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ var closeSpan = document.getElementsByClassName("close-span");
 
 for (var i = 0; i < closeSpan.length; i++) {
   closeSpan[i].addEventListener("click" ,function(e){
+    e.preventDefault();
 
     divIma.setAttribute("class","oculto");
 
